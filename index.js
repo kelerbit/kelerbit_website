1b546cc24aa39c7528564ef6762bf0600e3f41ad
Viktor K <kelerbit@gmail.com>
2025-07-13 12:56:09 +0300

    Go Back button

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
